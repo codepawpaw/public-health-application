c9bceb68019f9a3d54c598808248d486bfef2e51
Jonathan <jonathannael12@gmail.com>
2017-04-26 09:54:33 +0000

    set map width to be dynamic

--- public/js/directives.js
+++ public/js/directives.js
@@ -7,4 +7,18 @@ angular.module('app.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
+  }).directive('dynamicMap', function ($window) {
+  	console.log("called");
+    return {
+        restrict: 'A',
+
+        link: function (scope, elem, attrs) {
+
+            var winHeight = $window.innerHeight;
+
+            var headerHeight = attrs.banner ? attrs.banner : 0;
+
+            elem.css('height', winHeight - headerHeight + 'px');
+        }
+    };
   });
